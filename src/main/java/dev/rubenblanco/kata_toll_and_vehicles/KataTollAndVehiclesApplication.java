c9987662d11f8a2513a7693226e11767c3ec4844
Rubén Blanco <r.blanco.3d@gmail.com>
2024-11-22 00:08:49 +0100

    Springboot project initialization

---
+++ src/main/java/dev/rubenblanco/kata_toll_and_vehicles/KataTollAndVehiclesApplication.java
@@ -0,0 +1,13 @@
+package dev.rubenblanco.kata_toll_and_vehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataTollAndVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataTollAndVehiclesApplication.class, args);
+	}
+
+}
