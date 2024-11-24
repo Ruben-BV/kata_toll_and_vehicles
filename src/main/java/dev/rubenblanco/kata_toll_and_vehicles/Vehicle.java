f9c4a0310c33054e3a215d9db0e6fda17723dff2
Rubén Blanco <r.blanco.3d@gmail.com>
2024-11-24 20:41:20 +0100

    Variable number OfAxis was added

--- src/main/java/dev/rubenblanco/kata_toll_and_vehicles/Vehicle.java
+++ src/main/java/dev/rubenblanco/kata_toll_and_vehicles/Vehicle.java
@@ -5,36 +5,39 @@ public class Vehicle {
     long vehicleId;
     VehicleType vehicleType;
     String licensePlate;
-    
-    public Vehicle(long vehicleId, VehicleType vehicleType, String licensePlate) {
+    int numberOfAxis;
+    public Vehicle(long vehicleId, VehicleType vehicleType, String licensePlate, int numberOfAxis) {
         this.vehicleId = vehicleId;
         this.vehicleType = vehicleType;
         this.licensePlate = licensePlate;
+        this.numberOfAxis = numberOfAxis;
     }
-
     public long getVehicleId() {
         return vehicleId;
     }
-
     public void setVehicleId(long vehicleId) {
         this.vehicleId = vehicleId;
     }
-
     public VehicleType getVehicleType() {
         return vehicleType;
     }
-
     public void setVehicleType(VehicleType vehicleType) {
         this.vehicleType = vehicleType;
     }
-
     public String getLicensePlate() {
         return licensePlate;
     }
-
     public void setLicensePlate(String licensePlate) {
         this.licensePlate = licensePlate;
     }
+    public int getNumberOfAxis() {
+        return numberOfAxis;
+    }
+    public void setNumberOfAxis(int numberOfAxis) {
+        this.numberOfAxis = numberOfAxis;
+    }
+    
+    
     
 }
 
